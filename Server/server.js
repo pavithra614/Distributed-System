eed0ad4e64aa922b72a59dca60c87441e250c226
Nimeshika Dilshani <45098384+tashidu@users.noreply.github.com>
2025-02-02 20:31:22 +0530

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -17,5 +17,5 @@ app.use('/api/polls', voteRoutes); // This ensures vote routes are under `/api/p
 
 
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
